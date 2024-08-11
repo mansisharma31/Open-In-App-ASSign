7f5dcbd8c4514b9749873ec45b7d53b4cdda638b
Mansi <mansisharma3120@gmail.com>
2024-08-11 23:09:57 +0530

    changes done

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GoogleOAuthProvider clientId="968478220167-gp6r8ptbard5uqheprjoj2pvivtvs4hv.apps.googleusercontent.com">
     <App />
+    </GoogleOAuthProvider>
+    
   </React.StrictMode>
 );
 
@@ -15,3 +19,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
